dc7065a8311d32bd07c89c8b465f9a1cf08bba19
Calvin <63000172+calvintettey@users.noreply.github.com>
2020-09-23 14:30:39 +0000

    adding data to firestore

--- src/store/allusersActions.js
+++ src/store/allusersActions.js
@@ -1,22 +1,29 @@
 export const addUser = (user) => {
-    return {
-        type: "ADD_USER",
-        payload: user
-    }
-}
-
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("All Users")
+      .add(user)
+      .then((doc) => {
+        dispatch({
+            type: "ADD_USER",
+            payload: user
+        })
+      });
+  };
+  
+};
 
 export const deleteUser = (user_id) => {
-    return {
-        type: "DELETE_USER",
-        payload: user_id
-    }
-}
+  return {
+    type: "DELETE_USER",
+    payload: user_id,
+  };
+};
 
 export const editUser = (user_id, updated_info) => {
-    return {
-        type: "EDIT_USER",
-        user_id: user_id, 
-        updated_info: updated_info
-    }
-}
+  return {
+    type: "EDIT_USER",
+    user_id: user_id,
+    updated_info: updated_info,
+  };
+};
